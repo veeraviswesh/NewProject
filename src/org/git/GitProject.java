deeff2fe8b4d5664851824632d7a6da8fa705c77
veeraviswesh <veeraviswesh@gmail.com>
2022-03-28 14:43:18 +0530

    hello

---
+++ src/org/git/GitProject.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class GitProject {
+	public void java() {
+		System.out.println("java project ");
+
+	}
+	public static void main(String[] args) {
+		GitProject git = new GitProject();
+		git.java();
+	}
+}
